532917e1ce3113ac948d1215f8b8663386130e4c
carlosvale03 <dovalecarlos03@gmail.com>
2026-04-15 19:22:36 -0300

    setup: configurar entrypoint e dockerfile para deploy no render

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Falhar o script se algum comando der erro
+set -e
+
+echo "📍 Rodando Migrations..."
+python manage.py migrate --noinput
+
+echo "📍 Coletando arquivos estáticos..."
+python manage.py collectstatic --noinput
+
+echo "🚀 Iniciando Gunicorn..."
+exec gunicorn core.wsgi:application --bind 0.0.0.0:8000
+
